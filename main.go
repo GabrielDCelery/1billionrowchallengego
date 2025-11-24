30d70d8c7e436118a53a4380258b0b3a78c1ec54
GaborZeller <gabriel.d.celery@gmail.com>
2025-11-24 22:37:57 +0000

    feat: added basic script for reading and parsing measurements

--- main.go
+++ main.go
@@ -1,3 +1,58 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const bufferSize = 2048
+
+func main() {
+	filePath := "./measurements.txt"
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatalf("failed to open %s, reason: %v", filePath, err)
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, bufferSize)
+	validBytesInBuffer := 0
+
+	for {
+		numOfBytesRead, errRead := file.Read(buffer[validBytesInBuffer:])
+
+		validBytesInBuffer += numOfBytesRead
+
+		if validBytesInBuffer > bufferSize-1 {
+			log.Fatalf("exceeded buffer size of %d", bufferSize)
+		}
+
+		numOfBytesParsed, errParse := parse(buffer[:validBytesInBuffer])
+
+		if errParse != nil {
+			log.Fatalf("failed ot parse")
+		}
+
+		if numOfBytesParsed > 0 {
+			copy(buffer, buffer[numOfBytesParsed:validBytesInBuffer])
+			validBytesInBuffer -= numOfBytesParsed
+		}
+
+		if errRead == io.EOF {
+			break
+		}
+
+		if errRead != nil {
+			log.Fatalf("failed to read file, reason: %v", err)
+		}
+	}
+}
+
+func parse(chunk []byte) (int, error) {
+	fmt.Println(string(chunk))
+	return len(chunk), nil
+}
